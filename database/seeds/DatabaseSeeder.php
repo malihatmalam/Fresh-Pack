6ee9e8513000159ff0fc14720bda44b5c6b5b69d
Malihatmalam <malihatmalam@gmail.com>
2021-01-23 10:05:48 +0700

    Seeder Database (City and Province)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Menjalankan seeder  provinsi, kota dan kecamatan
+        $this->call(ProvinceTableSeeder::class);
+        $this->call(CityTableSeeder::class);
+        // $this->call(DistrictTableSeeder::class);
+        // Tambahan untuk ngedummy user 
+        // $this->call(UsersTableSeeder::class); 
     }
 }
